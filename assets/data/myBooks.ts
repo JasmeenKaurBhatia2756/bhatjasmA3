096ee7fe2f91afe626481228957175c6b7e53da6
JasmeenKaurBhatia2756 <68974565+JasmeenKaurBhatia2756@users.noreply.github.com>
2020-07-29 19:05:23 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,8 @@
+import { bookInfo } from  '../../app/bookInfo';
+
+export const MYBOOKS: bookInfo[] = [
+{ bname: 'You Can Win', bauthor: 'Shiv Khera', bgenre: 'Inspirational', byear: 1998, bpicture: 'assets/images/You-Can-Win-Shiv-Khera.jpg'},
+{ bname: 'Ofcourse! I Love You', bauthor: 'Durjoy Dutta', bgenre: 'Romantic', byear: 2008, bpicture: 'assets/images/ofcourse_i_love_you.jpg'},
+{ bname: 'She Broke Up I Didnt', bauthor: 'Durjoy Dutta', bgenre: 'Romantic', byear: 2010, bpicture: 'assets/images/she_broke_up.jpg'},
+{ bname: 'Worlds Best Boyfriend', bauthor: 'Durjoy Dutta', bgenre: 'Romantic', byear: 2015, bpicture: 'assets/images/worlds_best_boyfriend.jpg'},
+];
